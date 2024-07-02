13728b7b1fb35bd02279710733096132502137e8
Kuritiuday <128903498+Kuritiuday@users.noreply.github.com>
2024-07-02 18:16:34 +0530

    Add files via upload

---
+++ task3 python.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import classification_report, accuracy_score
+import matplotlib.pyplot as plt
+
+file_path = 'bank-additional-full.csv'
+data = pd.read_csv(file_path, sep=';')
+data = data.dropna()
+
+label_encoders = {}
+for column in data.select_dtypes(include=['object']).columns:
+    label_encoders[column] = LabelEncoder()
+    data[column] = label_encoders[column].fit_transform(data[column])
+
+X = data.drop('y', axis=1)
+y = data['y']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print("Classification Report:")
+print(report)
+
+plt.figure(figsize=(20,10))
+plot_tree(clf, feature_names=X.columns, class_names=label_encoders['y'].classes_, filled=True, rounded=True)
+plt.savefig("decision_tree.png")
+plt.show()
+
